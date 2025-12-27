a21482ba8ac4f84c7f13e811b0010a486eb261d3
Tung Nguyen <nvntung@gmail.com>
2025-12-27 18:33:56 +0000

    Create model and use migrations
    Commands:
    * python manage.py makemigrations
    * python manage.py showmigrations
    * python manage.py migrate

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name + " : " + self.cuisine + " - $" + str(self.price)
